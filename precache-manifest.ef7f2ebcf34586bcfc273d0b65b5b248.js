45d1a83ade9fb2d0fe94e7056ffcceba4b7315fe
quadrantbs <quadrantbs@gmail.com>
2022-06-13 18:08:11 +0700

    Updates

---
+++ precache-manifest.ef7f2ebcf34586bcfc273d0b65b5b248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c0a4a8c8efb6b200b41",
+    "url": "/TodoList/static/css/main.7b03327a.chunk.css"
+  },
+  {
+    "revision": "3c0a4a8c8efb6b200b41",
+    "url": "/TodoList/static/js/main.3c0a4a8c.chunk.js"
+  },
+  {
+    "revision": "1580cca746bba4e3322d",
+    "url": "/TodoList/static/js/1.1580cca7.chunk.js"
+  },
+  {
+    "revision": "e5fd00049e7ac7cce407",
+    "url": "/TodoList/static/js/runtime~main.e5fd0004.js"
+  },
+  {
+    "revision": "f957275485ab89f01f855671c942163e",
+    "url": "/TodoList/index.html"
+  }
+];
